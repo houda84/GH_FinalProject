a1741b8539fcc472b35c4f9f465ebcacba0d6a1b
Mahmoud <ma.abdel.mohsen@gmail.com>
2020-12-26 00:07:28 +0100

    initial commit

---
+++ GH_FinalProject/GH_FinalProjectInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace GH_FinalProject
+{
+    public class GH_FinalProjectInfo : GH_AssemblyInfo
+    {
+        public override string Name => "GH_FinalProject Info";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("03867DC0-10C2-49F1-8EFB-66A7335C6D2D");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
